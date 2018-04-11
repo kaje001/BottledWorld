413672119d8f31ad47a7ee3f76a623129197b10e
Kaje <kajetan.vonhollen@web.de>
2018-04-11 17:51:47 +0200

    Added Base Structure

---
+++ BottledTest/Assets/_Scripts/PlayerEvents.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerEvents : MonoBehaviour {
+
+	public PlayerController playCon;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+		if(transform.position.z>40){
+			playCon.ResetGame(); //Wenn der Spieler das Lvel schafft wird er zurückgesetzt
+		}
+	}
+	
+	void OnCollisionEnter(Collision other){
+		if(other.transform.tag == "Obstacle"){
+			playCon.CheckDeath(); //Bei einem Zusammenstoss mit einem Hinderniss wird im PlayerController gecheckt, ob noch leben verfügbar sind und ob schon ein Checkpoint activiert wurde
+		}
+		
+	}
+	
+	//Es werden alle Interactables abgefragt und dann in der PlayerController Class getriggered
+	void OnTriggerEnter(Collider other){
+		if(other.transform.tag == "Coin"){ //eine Muenze einsammeln
+			playCon.AddCoin(other.gameObject);
+		}
+		if(other.transform.tag == "ExtraLife"){ //extra Leben einsammeln
+			playCon.GetExtraLife(other.gameObject);
+		}
+		if(other.transform.tag == "Jump"){ //JumpTrigger anlaufen
+			playCon.PlayerJump();
+		}
+		if(other.transform.tag == "Slow"){ //wird noch nict verwendet
+			playCon.SetPlayerSpeed(0.8f); 
+		}
+		if(other.transform.tag == "NormalSpeed"){ //wird noch nicht verwendet
+			playCon.ResetPlayerSpeed();
+		}
+		if(other.transform.tag == "Speed"){ //SpeedBoost anlaufen
+			playCon.PlayerSpeedBoost();
+		}
+		if(other.transform.tag == "Checkpoint"){ //Checkpoint aktivieren
+			playCon.SetCheckpoint(other.gameObject);
+		}
+	}
+}
---
+++ BottledTest/Assets/_Scripts/TouchCon.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class TouchCon : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler {
+
+	public PlayerController playCon;
+	int lastX;
+	int screenX;
+	
+
+	void Start () {
+		screenX = Screen.width;
+		
+	}
+	
+	
+	public void OnPointerDown(PointerEventData data)
+    {
+		lastX = (int) data.position.x;
+    }
+	
+	 public void OnPointerUp(PointerEventData data)
+    {
+    }
+	
+	
+	public void OnDrag(PointerEventData data)
+    {
+		int x = (int) data.position.x;
+		float rot = x - lastX;
+		playCon.RotateWorld(rot/screenX);
+		
+		
+		lastX = x;
+    }
+	
+}
